d4ca0edfd740029a4a5a0c2674004b2fea17a93d
sammar abbass <sammarabbass416@gmail.com>
2026-06-14 22:39:18 +0500

    Initial commit

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://dyadvvydrluaoaxsoqlh.supabase.co'
-const supabaseKey = 'sb_publishable_yU534ownpVHWMl03umHv-g_EbC01UJ_'
+const supabaseUrl = 'https://mxbkzpaxbpkhiigitybh.supabase.co'
+const supabaseKey = 'sb_publishable_nNFftLOV0Du6_SEv0n96Tg_cdbjkKSm'
 export const supabase = createClient(supabaseUrl, supabaseKey)
